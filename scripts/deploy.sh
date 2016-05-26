b88c371f2f74169190f8e084fb1f8f5b15e7df32
cchandurkar <cchandurkar@gmail.com>
2016-05-26 16:54:21 -0400

    added travis support

---
+++ scripts/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -o errexit
+
+# Logging
+echo "Info:";
+echo "Branch: $TRAVIS_BRANCH";
+
+# Exit if not a master
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Not on master. Skipping Integration !!";
+  exit;
+else
+
+  # Remove Build Folder
+  rm -rf build
+
+  # Build
+  npm run build
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@${GITHUB_REF}.git"  gh-pages > /dev/null
+
+  # Copy Files
+  cp -r ./docs ./gh-pages
+  cp -a ./dist/. ./gh-pages
+
+  # cd into gh-pages
+  cd gh-pages
+
+  # push
+  git add --all
+  git commit -m "Auto generated documentation $TRAVIS_BUILD_NUMBER by Travis"
+  git push -fq origin gh-pages > /dev/null 2>&1
+
+  echo "gh-pages updated";
+
+  # Remove tempGHPages directory
+  rm -rf gh-pages
+
+fi
